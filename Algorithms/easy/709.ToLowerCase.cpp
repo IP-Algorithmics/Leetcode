6f61ef3ce7c4d1bc8faa602500bc2be2e67c8bfc
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-09-08 20:56:12 +0300

    Update 709.ToLowerCase.cpp

--- Algorithms/easy/709.ToLowerCase.cpp
+++ Algorithms/easy/709.ToLowerCase.cpp
@@ -2,6 +2,21 @@
  * Runtime: 4 ms, faster than 49.15% of C++ online submissions for ToLowerCase.
  * Memory Usage: 8.1 MB, less than 94.87% of C++ online submissions for
  * ToLowerCase.
+ *
+ * Implement function ToLowerCase() that has a string parameter str, and returns
+ * the same string in lowercase.
+ *
+ * Example 1:
+ * Input: "Hello"
+ * Output: "hello"
+ *
+ * Example 2:
+ * Input: "here"
+ * Output: "here"
+ *
+ * Example 3:
+ * Input: "LOVELY"
+ * Output: "lovely"
  */
 #include <algorithm>
 #include <iostream>
