98318dd659c13b32adfb4c18ebd725dab19f6f37
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-07-22 23:02:56 +0200

    Update 15.3-sum.ts

--- Typescript/medium/15.3-sum.ts
+++ Typescript/medium/15.3-sum.ts
@@ -1,29 +1,20 @@
-/**
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
 
-Notice that the solution set must not contain duplicate triplets.
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
 
- 
+// Example 2:
+// Input: nums = []
+// Output: []
+// Example 3:
+// Input: nums = [0]
+// Output: []
 
-Example 1:
-
-Input: nums = [-1,0,1,2,-1,-4]
-Output: [[-1,-1,2],[-1,0,1]]
-Example 2:
-
-Input: nums = []
-Output: []
-Example 3:
-
-Input: nums = [0]
-Output: []
- 
-
-Constraints:
-
-0 <= nums.length <= 3000
--105 <= nums[i] <= 105
-*/
+// Constraints:
+// 0 <= nums.length <= 3000
+// -105 <= nums[i] <= 105
 
 import { test } from '../helpers/test-functions';
 
@@ -213,6 +204,9 @@ test(
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     ]),
-    [[-1,0,1],[0,0,0]]
+    [
+        [-1, 0, 1],
+        [0, 0, 0],
+    ]
 );
 export {};
